7e197d905d82588544000aad83446605f180261e
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:33:22 +0300

    web

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,13 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/1499767087682.jpg','./imgs/kave+310.jpg'];
 var currentIndex = 0;
-
-function showNextPicture() {
+document.getElementsByTagName("img")[0].addEventListener("click",showNextPicture)
+function showNextPicture(event) {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
-
+  document.getElementsByTagName("img")[0].src = pictures[currentIndex]
   // YOUR TASK: Finish this function!
 }
